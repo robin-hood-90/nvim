7bc7e2eba53a08e15f7304657dbd2cdae0393bdf
robin-hood-90 <robinvolegov@gmail.com>
2026-01-13 22:01:41 +0530

    toggele term updated

--- lua/rishav/core/toggleTerm.lua
+++ lua/rishav/core/toggleTerm.lua
@@ -1,7 +1,6 @@
 -- Toggle terminal function
 local term_buf = nil
 local term_win = nil
-
 local function toggle_terminal()
     -- Check if terminal window is open
     if term_win and vim.api.nvim_win_is_valid(term_win) then
@@ -9,26 +8,21 @@ local function toggle_terminal()
         term_win = nil
         return
     end
-
     -- Create buffer if it doesn't exist
     if not term_buf or not vim.api.nvim_buf_is_valid(term_buf) then
         term_buf = vim.api.nvim_create_buf(false, true)
     end
-
     -- Create split window and set buffer
     vim.cmd("botright split")
     term_win = vim.api.nvim_get_current_win()
     vim.api.nvim_win_set_buf(term_win, term_buf)
-    vim.api.nvim_win_set_height(term_win, 15)
-
+    vim.api.nvim_win_set_height(term_win, 10)
     -- Start terminal if not already started
     if vim.bo[term_buf].buftype ~= "terminal" then
-        vim.fn.termopen(vim.env.SHELL or vim.o.shell)
+        vim.fn.jobstart(vim.env.SHELL or vim.o.shell, { term = true })
     end
-
     -- Enter insert mode
     vim.cmd("startinsert")
 end
-
 -- Set the keymap
 vim.keymap.set({ "n", "t" }, "<C-t>", toggle_terminal, { noremap = true, silent = true, desc = "Toggle terminal" })
