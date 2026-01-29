39f545f597d3fc98bda74673978568a6c6072430
robin-hood-90 <robinvolegov@gmail.com>
2026-01-30 01:08:56 +0530

    dap config chage

--- lua/rishav/plugins/nvim-dap.lua
+++ lua/rishav/plugins/nvim-dap.lua
@@ -94,7 +94,7 @@ return {
             only_first_definition = true,
             all_references = false,
             clear_on_continue = false,
-            display_callback = function(variable, options)
+            display_callback = function(variable, buf, stackframe, node, options)
                 if options.virt_text_pos == "inline" then
                     return " -> " .. variable.value:gsub("%s+", " ")
                 else
