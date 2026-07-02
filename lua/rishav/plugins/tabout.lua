af775495d9379978504abfb76520c14e87f37588
robin-hood-90 <robinvolegov@gmail.com>
2026-07-02 15:26:21 +0530

    tabout changes

--- lua/rishav/plugins/tabout.lua
+++ lua/rishav/plugins/tabout.lua
@@ -5,14 +5,14 @@ return {
     event = "InsertCharPre",
     config = function()
         require("tabout").setup({
-            tabkey = "", -- Disabled: Tab is handled by nvim-cmp's unified mapping
-            backwards_tabkey = "", -- Disabled: S-Tab is handled by nvim-cmp's unified mapping
-            act_as_tab = true, -- shift content if tab out is not possible
-            act_as_shift_tab = false, -- reverse shift content if tab out is not possible
-            default_tab = "<C-t>", -- shift default action (only at the beginning of a line)
+            tabkey = "",                 -- Disabled: Tab is handled by nvim-cmp's unified mapping
+            backwards_tabkey = "",       -- Disabled: S-Tab is handled by nvim-cmp's unified mapping
+            act_as_tab = true,           -- shift content if tab out is not possible
+            act_as_shift_tab = false,    -- reverse shift content if tab out is not possible
+            default_tab = "<C-t>",       -- shift default action (only at the beginning of a line)
             default_shift_tab = "<C-d>", -- reverse shift default action
             enable_backwards = true,
-            completion = false, -- not used in completion pum
+            completion = false,          -- not used in completion pum
             tabouts = {
                 { open = "'", close = "'" },
                 { open = '"', close = '"' },
